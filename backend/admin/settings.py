01f966a6e5d529dff4d416e1a6bc41cde2471c4b
jeanpetitt <jeanpetityvelos@gmail.com>
2023-03-31 09:43:07 +0100

    create des models users et fruits

--- backend/admin/settings.py
+++ backend/admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .database import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'vente'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,13 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DB,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+        'PORT': POSTGRES_PORT,
+        
     }
 }
 
--- backend/vente/models.py
+++ backend/vente/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Utilisateur(User):
+
+	def __str__(self):
+		return this.email
+
+# creation du model fruit
+class Fruit(models.Model):
+
+	QUALITY = [
+		('Frais', 'Frais'),
+		('Humide', 'Humide'),
+		('Sec', 'Sec')
+	]
+
+	name = models.CharField(max_length=50)
+	price = models.IntegerField()
+	quality = models.CharField(choices=QUALITY, max_length=20)
+
+	user = models.ManyToManyField(User)
